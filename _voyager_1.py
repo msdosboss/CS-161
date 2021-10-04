448b40c79fd953ef0e59ce0035a18e9c61d43e5b
msdosboss <jacobt0722@gmail.com>
2021-10-04 12:01:26 -0700

    Update _voyager_1.py

--- _voyager_1.py
+++ _voyager_1.py
@@ -1,7 +1,7 @@
 x=16,637,000,000.0
 print("how many days since 9/25/09?")
 y = int(input())
-float(x) = (y*38,241*24)
+x = (y*38,241*24)
 print (x,"miles") 
 print(x*1.609344,"Kilometers")
 print(x/92,955,887.6,"Astronomical Units")
