deac57a1251960f4016768e5cb79514790c74be2
msdosboss <jacobt0722@gmail.com>
2021-10-04 11:57:22 -0700

    Inserting files

---
+++ _voyager_1.py
@@ -0,0 +1,10 @@
+x=16,637,000,000.0
+print("how many days since 9/25/09?")
+y = int(input())
+float(x) = (y*38,241*24)
+print (x,"miles") 
+print(x*1.609344,"Kilometers")
+print(x/92,955,887.6,"Astronomical Units")
+print(x*1609.344*2/299,792,458*3600,"hours in a roundtrip at light speed")
+
+
