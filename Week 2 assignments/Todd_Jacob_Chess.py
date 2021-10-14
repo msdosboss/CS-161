cb077a7333f6d53036f2c4ad7df0d0d588fa7f24
msdosboss <jacobt0722@gmail.com>
2021-10-14 11:51:57 -0700

    Update Todd_Jacob_Chess.py

--- Week 2 assignments/Todd_Jacob_Chess.py
+++ Week 2 assignments/Todd_Jacob_Chess.py
@@ -5,4 +5,5 @@ while x < 64:
     x = x + 1
 
 print(y,"amount of grains")
-print(int(y*50/1000),"amount of grain in grams")
+print(int(y*50/1000),"amount of grain in grams")    #y amount of grain * 50 mg per grain / 1000 converting to grams
+print(254806*1000/y,"meters high in grain")     #254806 amount of square KM, 1000 converting to meters, lets say the grain is 1 square meter
